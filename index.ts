ffc2e9d6dd7e86bd56592587b22bc6b521519512
NousheenRazzak <nousheen461@gmail.com>
2024-04-06 16:03:17 +0500

    atm-machine-system

---
+++ index.ts
@@ -0,0 +1,47 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let myBalance = 10000;
+
+console.log("Current balance: $" + myBalance);
+
+let myPin = 1234;
+
+let pinAnswer = await inquirer.prompt(
+    {
+        name: "pin",
+        type: "number",
+        message: "Enter your Pin Code: "
+    }
+)
+
+if (pinAnswer.pin === myPin) {
+    console.log("Correct Pin Code :)");
+
+    let operationAns = await inquirer.prompt(
+        {
+            name: "operation",
+            type: "list",
+            message: "Please select an option: ",
+            choices: ["Withdraw cash", "Check balance"]
+        }
+    )
+
+    if (operationAns.operation === "Withdraw cash") {
+        let amountAns = await inquirer.prompt(
+            {
+                name: "amount",
+                type: "number",
+                message: "Enter amount: "
+            }
+        )
+
+        let finalAmount = myBalance - amountAns.amount;
+        console.log("Your remaining balance is $" + finalAmount);
+    } else {
+        console.log("Your current balance is: $10000");
+    }
+} else {
+    console.log("Incorrect Pin Code :(");
+}
